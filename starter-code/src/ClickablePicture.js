5f26df5a3cd22734aac6147a5e28843aab964973
Pavlitsa <pzafeiri@gmail.com>
2019-11-26 23:55:36 +0100

    image works

--- starter-code/src/ClickablePicture.js
+++ starter-code/src/ClickablePicture.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 
 class ClickablePicture extends Component {
+  //
   state = {
-    unclickedImage: this.props.img,
-    clickedImage: this.props.imgClicked
+    unclicked: true,
+    myImage: this.props.img
     //classes accept props
   };
 
@@ -13,21 +14,23 @@ class ClickablePicture extends Component {
   // the second one is a callback function, where I can see the updated
 
   handleClick = () => {
-    if (this.state.unclickedImage === false) {
+    if (this.state.unclicked === true) {
       this.setState({
-        clickedImage: true
+        unclicked: false,
+        myImage: this.props.imgClicked
       });
-    } else if (this.state.unclickedImage === true) {
+    } else if (this.state.unclicked === false) {
       this.setState({
-        clickedImage: false
+        unclicked: true,
+        myImage: this.props.img
       });
     }
   };
 
   // first thing we always do is set up a render function
   render() {
-    console.log("render function", this.state);
-    return <img onClick={handleClick} src={this.props.img} alt="" />;
+    // console.log("render function", this.state);
+    return <img onClick={this.handleClick} src={this.state.myImage} alt="image" />;
   }
 }
 
